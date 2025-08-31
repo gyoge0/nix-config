2ba343de8d23b962543f7bdf80d8165170b7aa5f
Yogesh Thambidurai <yogesh@gyoge.com>
2025-08-31 12:58:55 -0400

    add gyoge to trusted users in nix darwin
    Devenv needs to add cachix caches, but cannot do so since gyoge isn't
    trusted.

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -1,9 +1,15 @@
 { ... }:
 {
   flake.modules.darwin.nix = {
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "admin"
+        "gyoge"
+      ];
+    };
   };
 }
