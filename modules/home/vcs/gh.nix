fd8ae639d05d5a488705b0f889f7fc3786b0f8fb
Yogesh Thambidurai <yogesh@gyoge.com>
2025-09-25 17:55:47 -0400

    remove copilot in gh cli
    Copilot in CLI was deprecated; replacement is a seperate gh copilot cli
    tool, but that can be added later if needed. Claude Code works fine for
    now.

--- modules/home/vcs/gh.nix
+++ modules/home/vcs/gh.nix
@@ -5,7 +5,6 @@
       {
         programs.gh = {
           enable = true;
-          extensions = with pkgs; [ gh-copilot ];
           # gh cli will try to write to ~/.config/hosts.yml and ~/.config/settings.yml,
           # which isn't allowed if we configure them from home-manager.
         };
