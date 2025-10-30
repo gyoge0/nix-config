f341c62cacc944baf1b1dd8c2a20aeb4c4c376d0
Yogesh Thambidurai <yogesh@gyoge.com>
2025-10-30 16:25:24 -0400

    update flake.lock

--- modules/home/vcs/jj.nix
+++ modules/home/vcs/jj.nix
@@ -3,10 +3,16 @@
 {
   flake.modules = {
     homeManager.vcs =
-      { ... }:
+      { pkgs, ... }:
       {
         programs.jujutsu = {
           enable = true;
+          # https://github.com/NixOS/nixpkgs/issues/456113
+          package = pkgs.jujutsu.override {
+            rustPlatform = pkgs.rustPlatform // {
+              buildRustPackage = pkgs.rustPlatform.buildRustPackage.override { cargoNextestHook = null; };
+            };
+          };
           settings = {
             user = {
               name = lib.mkDefault "Yogesh Thambidurai";
