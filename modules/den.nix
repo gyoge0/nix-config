81ff6908d2b92f3f801cb3ca4dbee3abbbff9351
Yogesh Thambidurai <yogesh@gyoge.com>
2026-08-13 09:10:30 -0400

    den: remove template examples

--- modules/den.nix
+++ modules/den.nix
@@ -1,9 +1,5 @@
 { lib, ... }:
 {
-  den.hosts.x86_64-linux.igloo.users.alice = { };
-  den.hosts.aarch64-darwin.apple.users.alice = { };
-  den.homes.x86_64-linux.alice = { };
-
   # enable hm for all users
   den.schema.user.classes = lib.mkDefault [ "homeManager" ];
 }
