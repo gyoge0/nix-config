370920d12ec95583feffbce05f9bddb85e78bbc7
Yogesh Thambidurai <yogesh@gyoge.com>
2026-09-02 17:13:32 -0400

    discord: remove home manager install

--- modules/aspects/desktop/discord.nix
+++ modules/aspects/desktop/discord.nix
@@ -1,6 +1,15 @@
 {
   den.aspects.desktop.discord = {
-    homeManager.programs.discord.enable = true;
+    homeManager = { pkgs, ... }: {
+#      programs.discord = {
+#        enable = true;
+#        settings.SKIP_HOST_UPDATE = true;
+#        package = pkgs.discord.override {
+#          # todo: get rid of this python script
+#          #disableUpdates = false;
+#        };
+#      };
+    };
     darwin.homebrew.casks = [
       "discord"
     ];
